718d50febdb3874233d01e2864d81cab7b3e089a
Omkar Pednekar <74041929+OmkarPednekar@users.noreply.github.com>
2025-04-19 16:47:12 +0530

    basics added

--- cache.go
+++ cache.go
@@ -1,1 +1,43 @@
-package cache
+package jsonCache
+
+import (
+	"time"
+)
+
+type Cache struct {
+	store map[string]CacheElement
+}
+
+type CacheElement struct {
+	value  []byte
+	expiry time.Time
+}
+
+func New() *Cache {
+	//inialize a new cache engine
+	cache := &Cache{
+		store: make(map[string]CacheElement),
+	}
+	return cache
+}
+
+func (c *Cache) Set(key string, value []byte, ttl int) {
+	expiration := time.Now().Add(time.Duration(ttl) * time.Millisecond)
+	var toStore = &CacheElement{
+		value:  value,
+		expiry: expiration,
+	}
+	c.store[key] = *toStore
+}
+
+func (c *Cache) Get(key string) []byte {
+	entry, found := c.store[key]
+	if !found {
+		return nil
+	}
+	if time.Now().After(entry.expiry) {
+		delete(c.store, key)
+		return nil
+	}
+	return entry.value
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module jsoncache
+module github.com/OmkarPednekar/jsonCache
 
 go 1.24.1
